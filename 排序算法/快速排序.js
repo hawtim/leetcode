a1f96f8a152aa3ff216ee1c282dfca837d709553
kunkka <hawtimzhang@gmail.com>
2020-11-12 00:15:48 +0800

    feat: leetcode

--- 排序算法/快速排序.js
+++ 排序算法/快速排序.js
@@ -2,9 +2,8 @@
 // 它是一种分而治之的算法，通过递归的方式将数据依次分解为包含较小元素和较大元素的不同子序列
 
 // 一个元素作为基准值（pivot）
-
 function quickSort(arr) {
-  if (!arr.length) return []
+  if (arr.length < 2) return arr
   var lesser = []
   var greater = []
   var pivot = list[0]
@@ -17,3 +16,7 @@ function quickSort(arr) {
   }
   return quickSort(lesser).concat(pivot, quickSort(greater))
 }
+
+// 时间复杂度平均为 (2n + 2)logn => O(nlogn)
+// 最差为 1/2n^2 => O(n^ 2) （正序或者逆序的数组）
+// 多数情况下为小于 nlogn。
